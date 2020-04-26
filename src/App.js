35f2868b2220719778f993c99cbf64dff8070275
Kyle Tilley <katilley10@gmail.com>
2020-04-26 14:42:52 -0600

    passing coordinates properly to app

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import BoardMaker from './BoardMaker.jsx';
 
 
 class App extends React.Component {
-  constructor (props) {
-    super (props)
+  constructor(props) {
+    super(props)
     this.state = {
       board: [
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0]
-            ]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    console.log(e.target)
+  handleClick(coords) {
+    let rowCoord, colCoord;
+    if (coords < 10) {
+      rowCoord = 0;
+      colCoord = coords;
+    } else if (coords > 9) {
+      let string = coords.toString();
+      rowCoord = Number.parseInt(string[0]);
+      colCoord = Number.parseInt(string[1]);
+    }
+    console.log(rowCoord, '---', colCoord);
   }
 
   render() {
     return (
       <div className="App-header">
-        <div className="Board"><BoardMaker handleClick={this.handleClick}/></div>
+        <div className="Board"><BoardMaker handleClick={this.handleClick} /></div>
       </div>
-      )}
+    )
+  }
 }
 
 export default App;
--- src/Tile.jsx
+++ src/Tile.jsx
@@ -6,18 +6,17 @@ const Tile = (props) => {
     "gridRowStart": `${props.rowStart}`,
     "gridColumnStart": `${props.colStart}`
   }
-  let rowCoord, colCoord;
-  if (props.place < 10) {
-    rowCoord = 0;
-    colCoord = props.place;
-  } else if (props.place > 9) {
-    let string = props.place.toString();
-    rowCoord = Number.parseInt(string[0]);
-    colCoord = Number.parseInt(string[1]);
-  }
-  console.log(rowCoord, '---', colCoord);
+  // let rowCoord, colCoord;
+  // if (props.place < 10) {
+  //   rowCoord = 0;
+  //   colCoord = props.place;
+  // } else if (props.place > 9) {
+  //   let string = props.place.toString();
+  //   rowCoord = Number.parseInt(string[0]);
+  //   colCoord = Number.parseInt(string[1]);
+  // }
   return (
-    <div className="Tile" style={divStyle} onClick={props.handleClick}><p>hi</p></div>
+    <div className="Tile" style={divStyle} onClick={() => props.handleClick(props.place)} >{props.place}</div>
   )
 }
 
