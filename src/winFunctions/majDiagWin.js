4077f930f328a48b71a21b632df41fe3b79d9cb2
Kyle Tilley <katilley10@gmail.com>
2020-05-23 22:15:22 -0600

    finished major diagonal win function

--- src/winFunctions/majDiagWin.js
+++ src/winFunctions/majDiagWin.js
@@ -2,16 +2,19 @@ const majDiagWin = (board, coords, turn) => {
   const y = coords[0];
   const x = coords[1];
   const majorDiag = [];
+  let counter = 0;
   const counterTurn = (turn === 'R') ? 'B' : 'R';
   for (let i = -3; i <= 3; i += 1) {
     if ((x + i < 0 || x + i > 9) || (y + i < 0 || y + i > 9)) continue;
-    console.log(y + i, '  ', x + i)
-      console.log('here')
     majorDiag.push(board[y + i][x + i]);
-
   }
-
+  for (let k = 0; k < majorDiag.length; k ++) {
+    if (majorDiag[k] === turn) counter += 1;
+    if (majorDiag[k] === counterTurn) counter = 0;
+    if (counter === 4) return turn;
+  }
   console.log('major', majorDiag)
+  return null;
 }
 
 module.exports = majDiagWin;
